068552d69861e66661a8337f728f78dea6b29c7c
Bence <bsalgoi@gmail.com>
2024-09-12 13:57:50 +0200

    separet completed tasks

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,55 +1,81 @@
-import { useEffect, useState } from 'react';
-import { getTasks } from '../services/taskService';
+import { useEffect, useState } from "react";
+import { getTasks } from "../services/taskService";
 import CategoryFilter from "./filters/CategoryFilter.jsx";
-import TaskItem from './TaskItem';
+import TaskItem from "./TaskItem";
 import PriorityFilter from "./filters/PriorityFilter.jsx";
 
 export default () => {
-    const [tasks, setTasks] = useState([]);
-    const [categoryFilter, setCategoryFilter] = useState('All');
-    const [priorityFilter, setPriorityFilter] = useState('All');
+  const [tasks, setTasks] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState("All");
+  const [priorityFilter, setPriorityFilter] = useState("All");
 
-    useEffect(() => {
-        fetchTasks();
-    }, []);
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
-    const fetchTasks = async () => {
-        const { data } = await getTasks();
-        setTasks(data);
-    };
+  const fetchTasks = async () => {
+    const { data } = await getTasks();
+    setTasks(data);
+  };
 
-    const sortedTasks = tasks.sort((task1, task2) => {
-        if (task1.completed !== task2.completed) {
-            return task1.completed ? 1 : -1;
-        }
-        return new Date(task2.createdAt) - new Date(task1.createdAt);
-    });
+  const sortedTasks = tasks.sort((task1, task2) => {
+    if (task1.completed !== task2.completed) {
+      return task1.completed ? 1 : -1;
+    }
+    return new Date(task2.createdAt) - new Date(task1.createdAt);
+  });
 
-    const filteredTasks = sortedTasks.filter((task) => {
-        const categoryMatch = categoryFilter === 'All' || task.category === categoryFilter;
-        const priorityMatch = priorityFilter === 'All' || task.priority === priorityFilter;
-        return categoryMatch && priorityMatch;
-    });
+  const filteredTasks = sortedTasks.filter((task) => {
+    const categoryMatch =
+      categoryFilter === "All" || task.category === categoryFilter;
+    const priorityMatch =
+      priorityFilter === "All" || task.priority === priorityFilter;
+    return categoryMatch && priorityMatch;
+  });
 
-    return (
-        <>
-            <div className="flex flex-col items-center mt-8 w-full">
-                <div className="flex items-center gap-4 w-full">
-                    <CategoryFilter categoryFilter={categoryFilter} setCategoryFilter={setCategoryFilter} />
-                    <PriorityFilter priorityFilter={priorityFilter} setPriorityFilter={setPriorityFilter} />
-                </div>
-                {filteredTasks.length > 0 && (
-                    <div className="flex flex-col w-full mt-8 gap-4">
-                        {filteredTasks.map((task) => (
-                            <TaskItem key={task.id} task={task} refreshTasks={fetchTasks}/>
-                        ))}
-                    </div>
-                ) || (
-                    <div className="text-center text-gray-500 dark:text-gray-400 mt-8">
-                        No tasks found!
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  const todoTasks = filteredTasks.filter((task) => !task.completed);
+  const completedTasks = filteredTasks.filter((task) => task.completed);
+
+  return (
+    <div className="flex flex-col items-center mt-8 w-full px-4 max-w-screen-md mx-auto">
+      <div className="flex items-center gap-4 w-full">
+        <CategoryFilter
+          categoryFilter={categoryFilter}
+          setCategoryFilter={setCategoryFilter}
+        />
+        <PriorityFilter
+          priorityFilter={priorityFilter}
+          setPriorityFilter={setPriorityFilter}
+        />
+      </div>
+      <div className="flex flex-col w-full mt-8 gap-4">
+        <h2 className="text-2xl font-bold text-center dark:text-gray-400">
+          Tasks
+        </h2>
+        {todoTasks.length > 0 ? (
+          todoTasks.map((task) => (
+            <TaskItem key={task.id} task={task} refreshTasks={fetchTasks} />
+          ))
+        ) : (
+          <div className="text-center text-gray-500 dark:text-gray-400 mt-8">
+            No tasks found!
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col w-full mt-8 gap-4">
+        <h2 className="text-2xl font-bold text-center dark:text-gray-400">
+          Completed Tasks
+        </h2>
+        {completedTasks.length > 0 ? (
+          completedTasks.map((task) => (
+            <TaskItem key={task.id} task={task} refreshTasks={fetchTasks} />
+          ))
+        ) : (
+          <div className="text-center text-gray-500 dark:text-gray-400 mt-8">
+            No completed tasks!
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
